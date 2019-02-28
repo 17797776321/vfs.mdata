1c8e97cf0a433051d2e44aa02dab353bb162d93e
张宸 <531336750@qq.com>
2019-02-28 16:02:18 +0800

    上图

--- pages/Wisdom/index.vue
+++ pages/Wisdom/index.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="page-wisdom"></div>
+  <div class="page-wisdom">
+    <div class="banner1_box"></div>
+    <div class="banner2_box"></div>
+    <div class="banner3_box"></div>
+    <div class="banner4_box"></div>
+    <div class="banner5_box"></div>
+    <div class="banner6_box"></div>
+  </div>
 </template>
 <script>
 export default {
@@ -12,5 +19,25 @@ export default {
 }
 </script>
 <style lang="less">
-
+@import '../../assets/style/common.less';
+.page-wisdom{
+  .banner1_box{
+    .bgimg(750px,447px,'../images/wisdom1.png',100%,100%,center,center);
+  }
+  .banner2_box{
+    .bgimg(750px,247px,'../images/wisdom2.png',100%,100%,center,center);
+  }
+  .banner3_box{
+    .bgimg(750px,273px,'../images/wisdom3.png',100%,100%,center,center);
+  }
+  .banner4_box{
+    .bgimg(750px,274px,'../images/wisdom4.png',100%,100%,center,center);
+  }
+  .banner5_box{
+    .bgimg(750px,274px,'../images/wisdom5.png',100%,100%,center,center);
+  }
+  .banner6_box{
+    .bgimg(750px,275px,'../images/wisdom6.png',100%,100%,center,center);
+  }
+}
 </style>
