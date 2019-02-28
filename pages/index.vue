7cd840c4b170e3f6f5490a69984a7ccdd2fa07f3
张宸 <531336750@qq.com>
2019-02-28 11:20:50 +0800

    智慧数据

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,11 @@
 <template>
   <section class="page-home">
     <div class="banner_box1">
+      <h2 class="tit_cn">海量数据 引领未来</h2>
+      <p class="tit_en">Massive Data Leads the Future</p>
+      <div class="btn_group">
+        <a href="javascript:void(0);" class="wisdom_data">智慧数据</a><a href="javascript:void(0);" class="contact_us">联系我们</a>
+      </div>
     </div>
     <div class="banner_box2">
     </div>
@@ -26,6 +31,36 @@ export default {
 .page-home{
   .banner_box1{
     .bgimg(750px,975px,'../images/banner1.png',100%,100%,center,center);
+    text-align: center;
+    .tit_cn{
+      padding-top: 300px;
+      font-size: 69px;
+      color: #fefeff;
+      line-height: 90px;
+      
+    }
+    .tit_en{
+      font-size: 36px;
+      color: #fefeff;
+      line-height: 50px;
+    }
+    .btn_group{
+      padding-top: 58px;
+      text-align: center;
+      a{
+        display: inline-block;
+        width: 256px;
+        line-height: 80px;
+        font-size: 40px;
+        color: #ffffff;
+        border: 2px solid #ffffff;
+      }
+      .wisdom_data{
+        border-color: #ffcc00;
+        color: #ffcc00;
+        margin-right: 66px;
+      }
+    }
   }
   .banner_box2{
     .bgimg(750px,940px,'../images/banner2.png',100%,100%,center,center);
