096418d6c2011e682c97272276331dfef0c6dd92
张宸 <531336750@qq.com>
2019-02-28 16:40:32 +0800

    智慧数据

--- pages/Wisdom/index.vue
+++ pages/Wisdom/index.vue
@@ -1,11 +1,64 @@
 <template>
   <div class="page-wisdom">
-    <div class="banner1_box"></div>
-    <div class="banner2_box"></div>
-    <div class="banner3_box"></div>
-    <div class="banner4_box"></div>
-    <div class="banner5_box"></div>
-    <div class="banner6_box"></div>
+    <div class="banner1_box">
+      <h2>以智慧数据为基础，立足行业新场景</h2>
+    </div>
+    <div class="banner2_box">
+      <div class="header">
+        <h2>大数据</h2>
+        <h3>云端自动拓客</h3>
+        <h3>挖掘上百万精准客户</h3>
+      </div>
+      <div class="main">
+        <p>全天候服务</p>
+        <p>云端24小时自动拓客</p>
+        <p>不断挖掘行业精准客户</p>
+      </div>
+    </div>
+    <div class="banner3_box">
+      <div class="header">
+        <h2>小微数据</h2>
+        <h3>基于微信的Social-CRM系统</h3>
+      </div>
+      <div class="main">
+        <p>基于微信8亿用户信息</p>
+        <p>帮助企业与客户建立强连接</p>
+        <p>提升生意转化率的SasS级社会化客户关系管理平台</p>
+      </div>
+    </div>
+    <div class="banner4_box">
+      <div class="header">
+        <h2>用户画像</h2>
+        <h2>精准客户分群</h2>
+      </div>
+      <div class="main">
+        <p>4大类别30万标签词库</p>
+        <p>社交数据+用户行为数据+多平台数据</p>
+        <p>建模用户分析,洞察消费倾向</p>
+        <p>精准客户分群和精准营销</p>
+      </div>
+    </div>
+    <div class="banner5_box">
+      <div class="header">
+        <h2>拓展新客户</h2>
+      </div>
+      <div class="main">
+        <p>云端全天24小时自动拓客</p>
+        <p>通过行业微信群添加精准客户</p>
+        <p>导入数据库中</p>
+      </div>
+    </div>
+    <div class="banner6_box">
+      <div class="header">
+        <h2>大数据分析</h2>
+      </div>
+      <div class="main">
+        <p>通过人工智能语义分析、扫描每个微信好友的朋友圈</p>
+        <p>给每个好友不断标签化</p>
+        <p>最终形成精准的用户画像</p>
+        <p>实现精准定向营销</p>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -23,21 +76,148 @@ export default {
 .page-wisdom{
   .banner1_box{
     .bgimg(750px,447px,'../images/wisdom1.png',100%,100%,center,center);
+    h2{
+      font-size: 30px;
+      color: #ffffff;
+      text-align: center;
+      line-height: 44px;
+      padding-top: 198px;
+    }
   }
   .banner2_box{
-    .bgimg(750px,247px,'../images/wisdom2.png',100%,100%,center,center);
+    .bgimg(750px,367px,'../images/wisdom2.png',100%,100%,center,center);
+    .header{
+      padding-top: 80px;
+      padding-left: 48px;
+      h2{
+        font-size: 34px;
+        font-weight: 500;
+        color: #303030;
+        line-height: 46px;
+      }
+      h3{
+        font-size: 34px;
+        font-weight: 300;
+        color: #303030;
+      }
+    }
+    .main{
+      padding-top: 15px;
+      padding-left: 48px;
+      p{
+        font-size: 20px;
+        color: #838383;
+        line-height: 30px;
+      }
+    }
   }
   .banner3_box{
-    .bgimg(750px,273px,'../images/wisdom3.png',100%,100%,center,center);
+    .bgimg(750px,365px,'../images/wisdom3.png',100%,100%,center,center);
+    .header{
+      padding-top: 80px;
+      padding-left: 48px;
+      h2{
+        font-size: 34px;
+        font-weight: 500;
+        color: #303030;
+        line-height: 46px;
+      }
+      h3{
+        font-size: 34px;
+        font-weight: 300;
+        color: #303030;
+      }
+    }
+    .main{
+      padding-top: 15px;
+      padding-left: 48px;
+      p{
+        font-size: 20px;
+        color: #838383;
+        line-height: 30px;
+      }
+    }
   }
   .banner4_box{
-    .bgimg(750px,274px,'../images/wisdom4.png',100%,100%,center,center);
+    .bgimg(750px,365px,'../images/wisdom4.png',100%,100%,center,center);
+    .header{
+      padding-top: 80px;
+      padding-left: 48px;
+      h2{
+        font-size: 34px;
+        font-weight: 500;
+        color: #303030;
+        line-height: 46px;
+      }
+      h3{
+        font-size: 34px;
+        font-weight: 300;
+        color: #303030;
+      }
+    }
+    .main{
+      padding-top: 15px;
+      padding-left: 48px;
+      p{
+        font-size: 20px;
+        color: #838383;
+        line-height: 30px;
+      }
+    }
   }
   .banner5_box{
-    .bgimg(750px,274px,'../images/wisdom5.png',100%,100%,center,center);
+    .bgimg(750px,367px,'../images/wisdom5.png',100%,100%,center,center);
+    .header{
+      padding-top: 80px;
+      padding-left: 48px;
+      h2{
+        font-size: 34px;
+        font-weight: 500;
+        color: #303030;
+        line-height: 46px;
+      }
+      h3{
+        font-size: 34px;
+        font-weight: 300;
+        color: #303030;
+      }
+    }
+    .main{
+      padding-top: 15px;
+      padding-left: 48px;
+      p{
+        font-size: 20px;
+        color: #838383;
+        line-height: 30px;
+      }
+    }
   }
   .banner6_box{
-    .bgimg(750px,275px,'../images/wisdom6.png',100%,100%,center,center);
+    .bgimg(750px,366px,'../images/wisdom6.png',100%,100%,center,center);
+    .header{
+      padding-top: 80px;
+      padding-left: 48px;
+      h2{
+        font-size: 34px;
+        font-weight: 500;
+        color: #303030;
+        line-height: 46px;
+      }
+      h3{
+        font-size: 34px;
+        font-weight: 300;
+        color: #303030;
+      }
+    }
+    .main{
+      padding-top: 15px;
+      padding-left: 48px;
+      p{
+        font-size: 20px;
+        color: #838383;
+        line-height: 30px;
+      }
+    }
   }
 }
 </style>
