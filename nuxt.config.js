30766fed4c73988024f05c295d0627da58a17468
张宸 <531336750@qq.com>
2019-02-28 09:20:43 +0800

    初始化

---
+++ nuxt.config.js
@@ -0,0 +1,66 @@
+const pkg = require('./package')
+
+
+module.exports = {
+  mode: 'universal',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    script: [
+      { src: '/lib/flexible.js', type: 'text/javascript', charset: 'utf-8'}
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+    'normalize.css',
+    '~assets/style/global.css'
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+  ],
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+      
+    },
+    postcss: [
+      require('postcss-px2rem')({
+        remUnit: 75
+      })
+    ]
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="page-home">
+    111
+  </section>
+</template>
+
+<script>
+
+export default {
+  components:{},
+  name:'Home',
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style>
+.page-home{
+  width: 750px;
+  height: 100px;
+  background-color: #ff0000
+}
+</style>
