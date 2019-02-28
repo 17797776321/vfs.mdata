1a0c1a353c07106444015a2e257c5e26dcd99884
张宸 <531336750@qq.com>
2019-02-28 11:45:04 +0800

    智慧数据

--- pages/index.vue
+++ pages/index.vue
@@ -8,6 +8,16 @@
       </div>
     </div>
     <div class="banner_box2">
+      <div class="content_box">
+        <div class="analysis_box">
+          <a href="javascript:void(0);" class="pos1">大数据</a>
+          <a href="javascript:void(0);" class="pos2">提高盈利</a>
+          <a href="javascript:void(0);" class="pos3">吸引顾客</a>
+          <a href="javascript:void(0);" class="pos4">消费数据</a>
+          <a href="javascript:void(0);" class="pos5">人工智能</a>
+          <h2 class="center_text">智慧数据分析</h2>
+        </div>
+      </div>
     </div>
     <div class="banner_box3">
     </div>
@@ -64,6 +74,48 @@ export default {
   }
   .banner_box2{
     .bgimg(750px,940px,'../images/banner2.png',100%,100%,center,center);
+    .content_box{
+      padding-top: 210px;
+      .analysis_box{
+        .bgimg(527px,528px,'../images/bcircle.png',100%,100%,center,center);
+        margin: 0 auto;
+        position: relative;
+        text-align: center;
+        a{
+          display: inline-block;
+          .bgimg(107px,107px,'../images/scircle.png',100%,100%,center,center);
+          font-size: 20px;
+          color:#172027;
+          line-height: 107px;
+          position: absolute;
+        }
+        .pos1{
+          left: 210px;
+          top: -52px;
+        }
+        .pos2{
+          left: -38px;
+          top: 130px;
+        }
+        .pos3{
+          left: 455px;
+          top: 130px;
+        }
+        .pos4{
+          left: 45px;
+          top: 405px;
+        }
+        .pos5{
+          left: 374px;
+          top: 405px;
+        }
+        .center_text{
+          font-size: 34px;
+          color: #ffffff;
+          padding-top: 230px;
+        }
+      }
+    }
   }
   .banner_box3{
     .bgimg(750px,1600px,'../images/banner3.png',100%,100%,center,center);
