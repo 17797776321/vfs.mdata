19677f5371ad5a5e9a03ab393b498e6925b9246e
张宸 <531336750@qq.com>
2019-02-28 14:51:54 +0800

    创建关于我们&智慧数据页

---
+++ pages/About/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="page-about">关于</div>
+</template>
+<script>
+export default {
+  name: 'About',
+  data() {} 
+}
+</script>
+<style lang="less">
+
+</style>
---
+++ pages/Wisdom/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="page-wisdom"></div>
+</template>
+<script>
+export default {
+  name: 'Wisdom',
+  data() {
+    return{}
+  }
+}
+</script>
+<style lang="less">
+
+</style>
