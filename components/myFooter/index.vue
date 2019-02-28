d75c101cf6b33f94c4343684b245794730a638a3
张宸 <531336750@qq.com>
2019-02-28 10:52:41 +0800

    添加页脚

---
+++ components/myFooter/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="my_footer"></div>
+</template>
+<script>
+export default {
+  data() {
+    return{}
+  }
+}
+</script>
+<style lang="less">
+@import '../../assets/style/common.less';
+.my_footer{
+  .bgimg(750px,927px,'../images/banner5.png',100%,100%,center,center);
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,14 +2,17 @@
   <div class="layout">
     <my-nav></my-nav>
     <nuxt class="view_box"/>
+    <my-footer></my-footer>
   </div>
 </template>
 <script>
 import myNav from '@/components/myNav'
+import myFooter from '@/components/myFooter'
 export default {
   name: 'Layout',
   components:{
-    myNav
+    myNav,
+    myFooter
   }
 }
 </script>
