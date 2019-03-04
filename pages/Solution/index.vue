126bd1b712dca481c560e8a3c10992641dc674b4
张宸 <531336750@qq.com>
2019-03-04 09:41:32 +0800

    添加banner图

--- pages/Solution/index.vue
+++ pages/Solution/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="page-solution">解决方案页面</div>
+  <div class="page-solution">
+    <div class="banner_box">
+      <h2 class="tit_cn">当数据发出声音时，我们可以帮助您找到最佳倾听方式</h2>
+      <p class="tit_en">When data sounds, we can help you find the best way to listen.</p>
+    </div>
+  </div>
 </template>
 <script>
 export default {
@@ -10,5 +15,12 @@ export default {
 }
 </script>
 <style lang="less">
-.page-solution{}
+@import '../../assets/style/common.less';
+.page-solution{
+  .banner_box{
+   .bgimg(750px,447px,'../../assets/images/banner_so.png',100%,100%,center,center);
+    .tit_cn{}
+    .tit_en{}
+  }
+}
 </style>
